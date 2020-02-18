239c3d5ea0087a8ef34333a34ee22ea867897789
Eric <you@example.com>
2020-02-18 19:58:46 +0100

    change route call

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // On ajoute un prefix aux routes qu'on recupere via le require
 router.use("/api/user", require("./user"));
-router.use("/api/user", require("./good"));
-router.use("/api/user", require("./service"));
+router.use("/api/good", require("./good"));
+router.use("/api/service", require("./service"));
 
 module.exports = router;
