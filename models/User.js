cc36de4a5ab12bf9a0cb75f297106ca5023850c9
Ridha <rbenrhouma2@gmail.com>
2020-02-18 21:06:42 +0100

    test

--- models/User.js
+++ models/User.js
@@ -1,22 +1,24 @@
-const mongoose = require('mongoose');
+// test
 
-const UserModel = mongoose.model('User', {
-	account: {
-		username: {
-			type: String,
-			unique: true
-		},
-		biography: String
-	},
-	email: {
-		type: String,
-		unique: true
-	},
-	tokenNotification: String,
-	tokenConnexion: String,
-	hash: String,
-	salt: String,
-	lastConnexion: Date
+const mongoose = require("mongoose");
+
+const UserModel = mongoose.model("User", {
+  account: {
+    username: {
+      type: String,
+      unique: true
+    },
+    biography: String
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  tokenNotification: String,
+  tokenConnexion: String,
+  hash: String,
+  salt: String,
+  lastConnexion: Date
 });
 
 module.exports = UserModel;
